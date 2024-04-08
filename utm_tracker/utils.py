bdc64b9491e39e89056842c909144f1234735f60
om <om.kumar@manprax.com>
2024-04-08 18:20:51 +0530

    Fixed auto-enrollment issue on lead source table

--- utm_tracker/utils.py
+++ utm_tracker/utils.py
@@ -1,5 +1,6 @@
 
 from .models import LeadSource
+from django.conf import settings
 
 
 def update_leassourceenrollment(*args,**kwargs):
@@ -12,7 +13,11 @@ def update_leassourceenrollment(*args,**kwargs):
         lms_course_course_id = str(lms_course_enrollment.course_id)
     
     if lms_session_data:
-        lms_session_id = lms_session_data.COOKIES['sessionid']
+        # lms_session_id = lms_session_data.COOKIES['sessionid']
+        lms_session_name = settings.FEATURES.get('LMS_SESSION_ID', 'sessionid')
+        lms_session_id = lms_session_data.COOKIES[lms_session_name]
+
+
         get_leadsource_details = LeadSource.objects.filter(session_id= lms_session_id,course_id= lms_course_course_id)
         
     else:
